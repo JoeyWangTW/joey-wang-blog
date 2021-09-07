2beea6aeb041fc77b7be6caffc543b2ba1ee7cee
Joey Wang <joey840404@gmail.com>
2021-09-06 23:24:02 -0700

    Projects page update
    - update projects
    - change description

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Personal Site (Work In Progress)',
+    description: `personal website, portfolio. Still work in progress to build more on it.`,
+    imgSrc: '/static/images/personal-site.png',
+    href: 'https://www.szuyuwang.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Youtube Channel',
+    description: `A place where I share my life, work and tech`,
+    imgSrc: '/static/images/youtube.png',
+    href: 'https://www.youtube.com/user/TheJoeywang123',
   },
 ]
 
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Learn more about all the cool projects I've done
           </p>
         </div>
         <div className="container py-12">
